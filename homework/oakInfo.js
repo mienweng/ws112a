c6dce262a400b7760b6f1eb7f5fbaa87a4c13c50
mienweng <sb93129@gmail.com>
2023-10-21 18:35:04 +0800

    ws112a

---
+++ homework/oakInfo.js
@@ -0,0 +1,44 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  console.log('path=', ctx.request.url.pathname);
+  ctx.response.body = `
+
+  <html>
+  <body>
+    <h1>Hello!<h1>
+  </body>
+  </html>`
+
+  if (ctx.request.url.pathname == `/ nqu`) {
+    ctx.response.body = `
+    < html >
+    <body>
+      <a href="https://www.nqu.edu.tw/">金大網頁</a>
+    </body>
+    </html > `
+  }
+
+  else if (ctx.request.url.pathname == `/ nqu / csie`) {
+    ctx.response.body = `
+    < html >
+    <body>
+      <a href="https://csie.nqu.edu.tw/">金大資工系</a>
+    </body>
+    </html > `
+  }
+
+  else if (ctx.request.url.pathname == `/ to / nqu`) {
+    ctx.response.redirect('https://www.nqu.edu.tw/')
+  }
+
+  else if (ctx.request.url.pathname == `/ to / nqu / csie`) {
+    ctx.response.redirect('https://csie.nqu.edu.tw/')
+  }
+});
+
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
+
